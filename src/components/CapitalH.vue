8b18328827dcde93503357b1f12f96621d38cbfe
IvanPrz5 <maperLBR@gmail.com>
2023-02-03 18:06:30 -0600

    commitValida

--- src/components/CapitalH.vue
+++ src/components/CapitalH.vue
@@ -1,121 +1,146 @@
 <template>
   <v-container class="container">
-    <v-simple-table>
+    <v-simple-table :search="search" class="elevation-1">
       <template v-slot:top>
         <v-toolbar flat>
           <v-toolbar-title class="title">Capital Humano</v-toolbar-title>
           <v-divider class="mx-6" inset vertical></v-divider>
           <template>
-            <v-dialog>
+            <v-dialog v-model="dialog" max-width="650px">
               <template v-slot:activator="{ on, attrs }">
                 <v-btn color="success" dark v-bind="attrs" v-on="on">
                   <v-icon>mdi-plus</v-icon>
                 </v-btn>
                 <v-divider class="mx-6" inset vertical></v-divider>
+                <v-menu v-model="menu1" :close-on-content-click="false" :nudge-right="40" transition="scale-transition"
+                  offset-y min-width="auto">
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field v-model="search" label="Fecha De Pago" prepend-icon="mdi-calendar" readonly
+                      v-bind="attrs" v-on="on" hide-details></v-text-field>
+                  </template>
+                  <v-date-picker v-model="search" @input="menu1 = false" no-title scrollable></v-date-picker>
+                </v-menu>
                 <!-- <v-text-field v-model="search" append-icon="mdi-magnify" label="Buscar" hide-details>
-                  </v-text-field> -->
+                </v-text-field> -->
               </template>
               <v-card>
                 <v-card-title>
-                  <span class="text-h5">Agregar</span>
+                  <span class="text-h5">Nuevo Registro</span>
                 </v-card-title>
                 <v-card-text>
                   <v-container>
-                    <v-form class="container-form">
-                  <v-row class="form-calendar">
-                    <v-col cols="12" md="4">
-                      <v-text-field label="Concepto" v-model="editedItem.concepto" dense outlined></v-text-field>
-                    </v-col>
-                    <v-col cols="12" md="4">
-                      <v-text-field label="Fondo" v-model="editedItem.fondo" dense outlined></v-text-field>
-                    </v-col>
-                    <v-col cols="12" md="4">
-                      <v-text-field label="Num. De Oficio" v-model="editedItem.numeroOficio" dense
-                        outlined></v-text-field>
-                    </v-col>
-                  </v-row>
-                  <div class="calendar-div">
-                    <v-menu ref="menu1" :close-on-content-click="false" transition="scale-transition" offset-y
-                      min-width="auto">
-                      <template v-slot:activator="{ on, attrs }">
-                        <v-text-field v-model="dateRangeText" label="Fecha De Inicio - Fecha Fin"
-                          prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on"></v-text-field>
-                      </template>
-                      <v-date-picker v-model="dates" no-title range></v-date-picker>
-                    </v-menu>
-                    <v-spacer></v-spacer>
-                    <v-menu v-model="menu" :close-on-content-click="false" :nudge-right="40"
-                      transition="scale-transition" offset-y min-width="auto">
-                      <template v-slot:activator="{ on, attrs }">
-                        <v-text-field v-model="dateFechaPago" label="Fecha De Pago" prepend-icon="mdi-calendar" readonly
-                          v-bind="attrs" v-on="on"></v-text-field>
-                      </template>
-                      <v-date-picker v-model="dateFechaPago" @input="menu = false" no-title scrollable></v-date-picker>
-                    </v-menu>
-                  </div>
-                  <v-row>
-                    <v-col cols="12" md="4">
-                      <v-text-field @keypress="onlyNumber" label="Retención Isr" v-model="editedItem.retencionIsr" dense
-                        outlined></v-text-field>
-                    </v-col>
-                    <v-col cols="12" md="4">
-                      <v-text-field @keypress="onlyNumber" label="Ajuste Isr" v-model="editedItem.ajusteIsr" dense
-                        outlined></v-text-field>
-                    </v-col>
-                    <v-col cols="12" md="4">
-                      <v-text-field @keypress="onlyNumber" label="Subsidio + Ajuste de Subsidio"
-                        v-model="editedItem.subsidio" dense outlined>{{ calculaPago }}</v-text-field>
-                    </v-col>
-                  </v-row>
-                  <div class="main-div">
-                    <v-menu :close-on-content-click="false" :nudge-right="40" transition="scale-transition" offset-y
-                      min-width="auto">
-                      <template v-slot:activator="{ on, attrs }">
-                        <v-text-field v-model="dateFechaCaptura" label="Fecha De Captura" prepend-icon="mdi-calendar"
-                          readonly v-bind="attrs" v-on="on" v-show="false"></v-text-field>
-                      </template>
-                    </v-menu>
-                    <v-spacer></v-spacer>
-                    <v-text-field @keypress="onlyNumber" label="A Pagar" v-model="editedItem.pagar"
-                      readonly></v-text-field>
-                  </div>
-                  <v-btn color="success" @click="saveData">Ingresar</v-btn>
-                </v-form>
+                    <v-form ref="form" lazy-validation>
+                      <v-row class="form-calendar">
+                        <v-col cols="12" md="4">
+                          <v-text-field label="Concepto" v-model="editedItem.concepto" required dense
+                            outlined></v-text-field>
+                        </v-col>
+                        <v-col cols="12" md="4">
+                          <v-text-field label="Fondo" v-model="editedItem.fondo" required dense outlined></v-text-field>
+                        </v-col>
+                        <v-col cols="12" md="4">
+                          <v-text-field label="Num. De Oficio" v-model="editedItem.numeroOficio" required dense
+                            outlined></v-text-field>
+                        </v-col>
+                      </v-row>
+                      <v-row class="calendar-div">
+                        <v-col>
+                          <v-menu :close-on-content-click="false" transition="scale-transition" offset-y
+                            min-width="auto">
+                            <template v-slot:activator="{ on, attrs }">
+                              <v-text-field v-model="dateRangeText" label="Fecha De Inicio - Fecha Fin"
+                                prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on"></v-text-field>
+                            </template>
+                            <v-date-picker v-model="dates" no-title range></v-date-picker>
+                          </v-menu>
+                        </v-col>
+                        <v-col>
+                          <v-menu v-model="menu" :close-on-content-click="false" :nudge-right="40"
+                            transition="scale-transition" offset-y min-width="auto">
+                            <template v-slot:activator="{ on, attrs }">
+                              <v-text-field v-model="dateFechaPago" label="Fecha De Pago" prepend-icon="mdi-calendar"
+                                readonly required v-bind="attrs" v-on="on"></v-text-field>
+                            </template>
+                            <v-date-picker v-model="dateFechaPago" @input="menu = false" no-title
+                              scrollable></v-date-picker>
+                          </v-menu>
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col cols="12" md="4">
+                          <v-text-field @keypress="onlyNumber" label="Retención Isr" v-model="editedItem.retencionIsr"
+                            dense required outlined></v-text-field>
+                        </v-col>
+                        <v-col cols="12" md="4">
+                          <v-text-field @keypress="onlyNumber" label="Ajuste Isr" v-model="editedItem.ajusteIsr" dense
+                            required outlined></v-text-field>
+                        </v-col>
+                        <v-col cols="12" md="4">
+                          <v-text-field @keypress="onlyNumber" label="Subsidio + Ajuste de Subsidio"
+                            v-model="editedItem.subsidio" required dense outlined>{{ calculaPago }}</v-text-field>
+                        </v-col>
+                      </v-row>
+                      <div>
+                        <v-text-field @keypress="onlyNumber" label="A Pagar" v-model="editedItem.pagar" readonly
+                          required></v-text-field>
+                      </div>
+                      <v-row v-show="false" class="main-div">
+                        <v-col cols="12" md="4">
+                          <v-menu :close-on-content-click="false" :nudge-right="40" transition="scale-transition"
+                            offset-y min-width="auto">
+                            <template v-slot:activator="{ on, attrs }">
+                              <v-text-field v-model="dateFechaCaptura" label="Fecha De Captura"
+                                prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on"
+                                v-show="false"></v-text-field>
+                            </template>
+                          </v-menu>
+                        </v-col>
+                      </v-row>
+                    </v-form>
                   </v-container>
                 </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="error darken-1" text @click="close">
+                    Cancelar
+                  </v-btn>
+                  <v-btn color="blue darken-1" text @click="saveData">
+                    Guardar
+                  </v-btn>
+                </v-card-actions>
               </v-card>
             </v-dialog>
           </template>
         </v-toolbar>
-        <thead>
-          <tr>
-            <th class="text-left">Concepto</th>
-            <th class="text-left">Fondo</th>
-            <th class="text-left">Número de Oficio</th>
-            <th class="text-left">Fecha Inicio</th>
-            <th class="text-left">Fecha Fin</th>
-            <th class="text-left">Fecha Pago</th>
-            <th class="text-left">Retención Isr</th>
-            <th class="text-left">Ajuste Isr</th>
-            <th class="text-left">Subsidio</th>
-            <th class="text-left">Total a Pagar</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="item in desserts" :key="item.id">
-            <td>{{ item.concepto }}</td>
-            <td>{{ item.fondo }}</td>
-            <td>{{ item.numeroOficio }}</td>
-            <td>{{ item.fechaInicio }}</td>
-            <td>{{ item.fechaFin }}</td>
-            <td>{{ item.fechaPago }}</td>
-            <td>{{ item.retencionIsr }}</td>
-            <td>{{ item.ajusteIsr }}</td>
-            <td>{{ item.subsidioAjuste }}</td>
-            <td>{{ item.pagar }}</td>
-          </tr>
-        </tbody>
       </template>
+       <thead>
+        <tr>
+          <th class="text-left">Concepto</th>
+          <th class="text-left">Fondo</th>
+          <th class="text-left">Número de Oficio</th>
+          <th class="text-left">Fecha Inicio</th>
+          <th class="text-left">Fecha Fin</th>
+          <th class="text-left">Fecha Pago</th>
+          <th class="text-left">Retención Isr</th>
+          <th class="text-left">Ajuste Isr</th>
+          <th class="text-left">Subsidio</th>
+          <th class="text-left">Total a Pagar</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in desserts" :key="item.id">
+          <td>{{ item.concepto }}</td>
+          <td>{{ item.fondo }}</td>
+          <td>{{ item.numeroOficio }}</td>
+          <td>{{ item.fechaInicio }}</td>
+          <td>{{ item.fechaFin }}</td>
+          <td>{{ item.fechaPago }}</td>
+          <td>{{ item.retencionIsr }}</td>
+          <td>{{ item.ajusteIsr }}</td>
+          <td>{{ item.subsidioAjuste }}</td>
+          <td>{{ item.pagar }}</td>
+        </tr>
+      </tbody>
     </v-simple-table>
   </v-container>
 </template>
@@ -129,18 +154,24 @@ export default {
     // Calendario,
   },
   data: () => ({
+    concepto:"",
+    dialog: false,
+    search: "",
     numberRules: [
       (value) => value > 0 || "campo requerido",
       (value) => value > 0 || "El valor debe ser mayor a cero",
+      v => !!v || 'Name is required',
     ],
     result: [],
+    menu1: false,
     menu: false,
     dates: [],
     dateFechaPago: "",
-    dateFechaCaptura: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+    dateFechaCaptura: new Date(
+      Date.now() - new Date().getTimezoneOffset() * 60000
+    )
       .toISOString()
       .substr(0, 10),
-    search: "",
     headers: [
       { text: "Concepto", align: "start", value: "concepto" },
       { text: "Fondo", align: "start", value: "fondo" },
@@ -224,6 +255,7 @@ export default {
         .substr(0, 10);
     },
     getMapping() {
+      this.desserts.length = "";
       axios.get("http://localhost:8082/CapitalHumano").then((response) => {
         this.result = response.data.data;
         console.log(response.data);
@@ -245,7 +277,8 @@ export default {
       });
     },
     saveData: function () {
-      axios
+      if(this.editedItem.concepto != null){
+        axios
         .post("http://localhost:8082/CapitalHumano", {
           concepto: this.editedItem.concepto,
           fondo: this.editedItem.fondo,
@@ -260,11 +293,18 @@ export default {
           fechaCaptura: this.dateFechaCaptura,
         })
         .then((response) => {
-          console.log(this.dateFormatted);
-          console.log(this.dateFechaCaptura);
-          this.result = response.data.data;
+          this.getMapping();
+          this.close();
           console.log(response.data);
         });
+      }
+    },
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
     },
   },
 };
