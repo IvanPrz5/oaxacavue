a8e19cd0ff30aab354a7e253dc2980635765a9c3
IvanPrz5 <maperLBR@gmail.com>
2023-02-07 19:13:07 -0600

    commitRelaciones

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,23 @@
   <v-app>
     <v-main>
       <CapitalH />
+      <Timbrado />
+      <Finalizado />
     </v-main>
   </v-app>
 </template>
 
 <script>
 import CapitalH from "./components/CapitalH.vue";
+import Timbrado from "./components/Timbrado.vue";
+import Finalizado from "./components/Finalizado.vue";
 
 export default {
   name: "App",
   components: {
     CapitalH,
+    Timbrado,
+    Finalizado,
   },
   data: () => ({
     //
--- src/components/Timbrado.vue
+++ src/components/Timbrado.vue
@@ -3,10 +3,10 @@
     <v-simple-table :search="search" class="elevation-1">
       <template v-slot:top>
         <v-toolbar flat>
-          <v-toolbar-title class="title">Capital Humano</v-toolbar-title>
+          <v-toolbar-title class="title">Timbrados</v-toolbar-title>
           <v-divider class="mx-6" inset vertical></v-divider>
           <template>
-            <v-dialog v-model="dialog" max-width="650px">
+            <v-dialog v-model="dialog" max-width="750px">
               <template v-slot:activator="{ on, attrs }">
                 <v-btn color="success" dark v-bind="attrs" v-on="on">
                   <v-icon>mdi-plus</v-icon>
@@ -21,7 +21,7 @@
                   <v-date-picker v-model="search" @input="menu1 = false" no-title scrollable></v-date-picker>
                 </v-menu>
                 <!-- <v-text-field v-model="search" append-icon="mdi-magnify" label="Buscar" hide-details>
-                </v-text-field> -->
+                  </v-text-field> -->
               </template>
               <v-card>
                 <v-card-title>
@@ -32,14 +32,15 @@
                     <v-form ref="form" lazy-validation>
                       <v-row class="form-calendar">
                         <v-col cols="12" md="4">
-                          <v-text-field label="Concepto" v-model="editedItem.concepto" required dense
+                          <v-text-field label="Archivo" v-model="editedItem.archivo" required dense
                             outlined></v-text-field>
                         </v-col>
                         <v-col cols="12" md="4">
-                          <v-text-field label="Fondo" v-model="editedItem.fondo" required dense outlined></v-text-field>
+                          <v-text-field label="Archivo Timbrado" v-model="editedItem.archivoTimbrar" required dense
+                            outlined></v-text-field>
                         </v-col>
                         <v-col cols="12" md="4">
-                          <v-text-field label="Num. De Oficio" v-model="editedItem.numeroOficio" required dense
+                          <v-text-field label="Total de Empleados" v-model="editedItem.totalEmpleados" required dense
                             outlined></v-text-field>
                         </v-col>
                       </v-row>
@@ -68,32 +69,54 @@
                       </v-row>
                       <v-row>
                         <v-col cols="12" md="4">
-                          <v-text-field @keypress="onlyNumber" label="Retención Isr" v-model="editedItem.retencionIsr"
-                            dense required outlined></v-text-field>
+                          <v-text-field label="SNFC" v-model="editedItem.snfc" dense required outlined></v-text-field>
                         </v-col>
                         <v-col cols="12" md="4">
-                          <v-text-field @keypress="onlyNumber" label="Ajuste Isr" v-model="editedItem.ajusteIsr" dense
-                            required outlined></v-text-field>
+                          <v-text-field label="Estatus Timbrado" v-model="editedItem.status" dense required
+                            outlined></v-text-field>
                         </v-col>
-                        <v-col cols="12" md="4">
-                          <v-text-field @keypress="onlyNumber" label="Subsidio + Ajuste de Subsidio"
-                            v-model="editedItem.subsidio" required dense outlined>{{ calculaPago }}</v-text-field>
+                        <v-col>
+                          <v-menu v-model="menu2" :close-on-content-click="false" :nudge-right="40"
+                            transition="scale-transition" offset-y min-width="auto">
+                            <template v-slot:activator="{ on, attrs }">
+                              <v-text-field v-model="dateFechaSubida" label="Fecha De Subida"
+                                prepend-icon="mdi-calendar" readonly required v-bind="attrs" v-on="on"></v-text-field>
+                            </template>
+                            <v-date-picker v-model="dateFechaSubida" @input="menu2 = false" no-title
+                              scrollable></v-date-picker>
+                          </v-menu>
                         </v-col>
                       </v-row>
-                      <div>
-                        <v-text-field @keypress="onlyNumber" label="A Pagar" v-model="editedItem.pagar" readonly
-                          required></v-text-field>
-                      </div>
-                      <v-row v-show="false" class="main-div">
+                      <v-row>
                         <v-col cols="12" md="4">
-                          <v-menu :close-on-content-click="false" :nudge-right="40" transition="scale-transition"
-                            offset-y min-width="auto">
-                            <template v-slot:activator="{ on, attrs }">
-                              <v-text-field v-model="dateFechaCaptura" label="Fecha De Captura"
-                                prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on"
-                                v-show="false"></v-text-field>
-                            </template>
-                          </v-menu>
+                          <v-text-field label="Importe Isr" v-model="editedItem.importeIsr" dense required
+                            outlined></v-text-field>
+                        </v-col>
+                        <v-col cols="12" md="4">
+                          <v-text-field label="Neto" v-model="editedItem.neto" dense required outlined></v-text-field>
+                        </v-col>
+                        <v-col cols="12" md="4">
+                          <v-text-field label="Documento Contable" v-model="editedItem.documentoContable" dense required
+                            outlined></v-text-field>
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col cols="12" md="4">
+                          <v-text-field label="Número" v-model="editedItem.numero" dense required
+                            outlined></v-text-field>
+                        </v-col>
+                        <v-col cols="12" md="4">
+                          <v-text-field label="Número de Ejecuciones" v-model="editedItem.numEjecuciones" dense required
+                            outlined></v-text-field>
+                        </v-col>
+                        <v-col cols="12" md="4">
+                          <v-text-field label="Nómina" v-model="editedItem.nomina" dense required
+                            outlined></v-text-field>
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col cols="12">
+                          <v-textarea label="Observaciones" v-model="editedItem.nomina" name="input-7-4" outlined></v-textarea>
                         </v-col>
                       </v-row>
                     </v-form>
@@ -113,32 +136,44 @@
           </template>
         </v-toolbar>
       </template>
-       <thead>
+      <thead>
         <tr>
-          <th class="text-left">Concepto</th>
-          <th class="text-left">Fondo</th>
-          <th class="text-left">Número de Oficio</th>
+          <th class="text-left">Archivo</th>
+          <th class="text-left">Archivo Timbrado</th>
+          <th class="text-left">Empleados</th>
           <th class="text-left">Fecha Inicio</th>
           <th class="text-left">Fecha Fin</th>
           <th class="text-left">Fecha Pago</th>
-          <th class="text-left">Retención Isr</th>
-          <th class="text-left">Ajuste Isr</th>
-          <th class="text-left">Subsidio</th>
-          <th class="text-left">Total a Pagar</th>
+          <th class="text-left">SNFC</th>
+          <th class="text-left">Estatus Timbrado</th>
+          <th class="text-left">Fecha de Subida</th>
+          <th class="text-left">Importe Isr</th>
+          <th class="text-left">Neto</th>
+          <th class="text-left">Documento Contable</th>
+          <th class="text-left">Número</th>
+          <th class="text-left">Número de Ejecuciones</th>
+          <th class="text-left">Nómina</th>
+          <th class="text-left">Observaciones</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="item in desserts" :key="item.id">
-          <td>{{ item.concepto }}</td>
-          <td>{{ item.fondo }}</td>
-          <td>{{ item.numeroOficio }}</td>
+        <tr v-for="item in desserts" :key="item.idTimbrado">
+          <td>{{ item.archivo }}</td>
+          <td>{{ item.archivoTimbrar }}</td>
+          <td>{{ item.totalEmpleados }}</td>
           <td>{{ item.fechaInicio }}</td>
           <td>{{ item.fechaFin }}</td>
           <td>{{ item.fechaPago }}</td>
-          <td>{{ item.retencionIsr }}</td>
-          <td>{{ item.ajusteIsr }}</td>
-          <td>{{ item.subsidioAjuste }}</td>
-          <td>{{ item.pagar }}</td>
+          <td>{{ item.descripcionSNFC }}</td>
+          <td>{{ item.descripcionStatus }}</td>
+          <td>{{ item.fechaSubida }}</td>
+          <td>{{ item.importeIsr }}</td>
+          <td>{{ item.neto }}</td>
+          <td>{{ item.documentoContable }}</td>
+          <td>{{ item.numero }}</td>
+          <td>{{ item.numEjecuciones }}</td>
+          <td>{{ item.nomina }}</td>
+          <td>{{ item.observaciones }}</td>
         </tr>
       </tbody>
     </v-simple-table>
@@ -154,27 +189,29 @@ export default {
     // Calendario,
   },
   data: () => ({
-    concepto:"",
+    concepto: "",
     dialog: false,
     search: "",
     numberRules: [
       (value) => value > 0 || "campo requerido",
       (value) => value > 0 || "El valor debe ser mayor a cero",
-      v => !!v || 'Name is required',
+      (v) => !!v || "Name is required",
     ],
     result: [],
-    menu1: false,
     menu: false,
+    menu1: false,
+    menu2: false,
     dates: [],
     dateFechaPago: "",
+    dateFechaSubida: "",
     dateFechaCaptura: new Date(
       Date.now() - new Date().getTimezoneOffset() * 60000
     )
       .toISOString()
       .substr(0, 10),
     /* headers: [
-      { text: "Concepto", align: "start", value: "concepto" },
-      { text: "Fondo", align: "start", value: "fondo" },
+      { text: "Archivo", align: "start", value: "concepto" },
+      { text: "A", align: "start", value: "fondo" },
       { text: "Número De Oficio", align: "start", value: "numeroOficio" },
       { text: "Inicio", align: "start", value: "fechaInicio" },
       { text: "Fin", align: "start", value: "fechaFin" },
@@ -256,48 +293,53 @@ export default {
     },
     getMapping() {
       this.desserts.length = "";
-      axios.get("http://localhost:8082/CapitalHumano").then((response) => {
+      axios.get("http://localhost:8082/Timbrado").then((response) => {
         this.result = response.data.data;
         console.log(response.data);
         for (let i = 0; i < response.data.length; i++) {
           this.desserts.push({
-            concepto: response.data[i].concepto,
-            fondo: response.data[i].fondo,
-            numeroOficio: response.data[i].numeroOficio,
+            archivo: response.data[i].archivo,
+            archivoTimbrar: response.data[i].archivoTimbrar,
+            totalEmpleados: response.data[i].totalEmpleados,
             fechaInicio: response.data[i].fechaInicio,
             fechaFin: response.data[i].fechaFin,
             fechaPago: response.data[i].fechaPago,
-            retencionIsr: response.data[i].retencionIsr,
-            ajusteIsr: response.data[i].ajusteIsr,
-            subsidioAjuste: response.data[i].subsidioAjuste,
-            pagar: response.data[i].pagar,
-            fechaCaptura: response.data[i].fechaCaptura,
+            descripcionSNFC: response.data[i].catalogoSNFCEntity.descripcion,/*  */
+            importeIsr: response.data[i].importeIsr,
+            descripcionStatus: response.data[i].catalogoStatusEntity.descripcion,/*  */
+            fechaSubida: response.data[i].fechaSubida,
+            neto: response.data[i].neto,
+            numEjecuciones: response.data[i].numEjecuciones,
+            observaciones: response.data[i].observaciones,
+            documentoContable: response.data[i].documentoContable,
+            numero: response.data[i].numero,
+            nomina: response.data[i].nomina,
           });
         }
       });
     },
     saveData: function () {
-      if(this.editedItem.concepto != null){
+      /* if (this.editedItem.concepto != null) {
         axios
-        .post("http://localhost:8082/CapitalHumano", {
-          concepto: this.editedItem.concepto,
-          fondo: this.editedItem.fondo,
-          numeroOficio: this.editedItem.numeroOficio,
-          fechaInicio: this.dates[0],
-          fechaFin: this.dates[1],
-          fechaPago: this.dateFechaPago,
-          retencionIsr: this.editedItem.retencionIsr,
-          ajusteIsr: this.editedItem.ajusteIsr,
-          subsidioAjuste: this.editedItem.subsidio,
-          pagar: this.editedItem.pagar,
-          fechaCaptura: this.dateFechaCaptura,
-        })
-        .then((response) => {
-          this.getMapping();
-          this.close();
-          console.log(response.data);
-        });
-      }
+          .post("http://localhost:8082/Timbrado", {
+            concepto: this.editedItem.concepto,
+            fondo: this.editedItem.fondo,
+            numeroOficio: this.editedItem.numeroOficio,
+            fechaInicio: this.dates[0],
+            fechaFin: this.dates[1],
+            fechaPago: this.dateFechaPago,
+            retencionIsr: this.editedItem.retencionIsr,
+            ajusteIsr: this.editedItem.ajusteIsr,
+            subsidioAjuste: this.editedItem.subsidio,
+            pagar: this.editedItem.pagar,
+            fechaCaptura: this.dateFechaCaptura,
+          })
+          .then((response) => {
+            this.getMapping();
+            this.close();
+            console.log(response.data);
+          });
+      } */
     },
     close() {
       this.dialog = false;
@@ -309,7 +351,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-@import "../style/CapitalH.css";
-</style>
